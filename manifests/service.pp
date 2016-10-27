26e4c303fd7bd6c61db469cbc63616e82a6f425b
Nick Miller <nick.miller@onyxpoint.com>
2016-10-27 17:46:10 -0400

    (SIMP-1839) Add journald module
    In order to get the systemd journal to pass to rsyslog, it needs to be
    configured to pass to syslog. Default is to not.
    
    - SIMPed module
    - Wrote more spec tests
    - Added a few config options to the defaults
    - Added details to metadata.json
    - Added CentOS as a supported OS
    - Fixed lint errors
    
    SIMP-1839 #comment added journald module
    
    Change-Id: I30ab02a6f6f7d9a359186bee74bb1dc17c082905

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,6 @@
+# manage the systemd-journald service
 class journald::service {
-  service { ['systemd-journald']:
+  service { 'systemd-journald':
     ensure => running,
     enable => true,
   }
