dcbe38e445b3dd040ea22d45a27cc9bc24a85572
Nick Miller <nick.miller@onyxpoint.com>
2016-10-27 17:46:10 -0400

    (SIMP-1839) Add journald module
    In order to get the systemd journal to pass to rsyslog, it needs to be
    configured to pass to syslog. Default is to not.
    
    - SIMPed module
    - Wrote more spec tests
    - Added a few config options to the defaults
    - Added details to metadata.json
    - Added CentOS as a supported OS
    
    SIMP-1839 #comment added journald module
    
    Change-Id: I30ab02a6f6f7d9a359186bee74bb1dc17c082905

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,22 +4,21 @@
 #
 # === Parameters:
 #
-# $persist_log::                 By default, the journal stores log data in /run/log/journal/. 
-#                                Since /run/ is volatile, log data is lost at reboot. To make the
-#                                data persistent, it is sufficient to create /var/log/journal/ 
-#                                where systemd-journald will then store the data.
+# $persist_log:: By default, the journal stores log data in /run/log/journal/.
+#                Since /run/ is volatile, log data is lost at reboot. To make the
+#                data persistent, it is sufficient to create /var/log/journal/
+#                where systemd-journald will then store the data.
 #
-# $options::                     a hash with journald parameters
+# $options::     a hash with journald parameters
 #
 class journald (
   $persist_log = $journald::params::persist_log,
   $options = undef
 ) inherits journald::params {
-  if $::osfamily == 'RedHat' and versioncmp($::operatingsystemrelease, '7.0') >= 0 {
+  if $::osfamily in ['RedHat','CentOS'] and versioncmp($::operatingsystemrelease, '7.0') >= 0 {
     contain ::journald::config
     contain ::journald::service
 
-    Class['journald::config'] ~>
-    Class['journald::service']
+    Class['journald::config'] ~> Class['journald::service']
   }
 }
